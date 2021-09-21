f109533d5832cb8d4f437049d8cad29510bb26b1
valerieturgeon <valerie.turgeon.cem@gmail.com>
2021-09-21 09:15:12 -0400

    REFACTOR DataAccess DbContext
    Fractionner le projet : création du DataAccess

--- CrazyBooks/CrazyBooks_DataAccess/Data/CrazyBooksDbContext.cs
+++ CrazyBooks/CrazyBooks_DataAccess/Data/CrazyBooksDbContext.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace CrazyBooks.Models.Data
+namespace CrazyBooks_Models.Data
 {
   public class CrazyBooksDbContext: DbContext
   {
