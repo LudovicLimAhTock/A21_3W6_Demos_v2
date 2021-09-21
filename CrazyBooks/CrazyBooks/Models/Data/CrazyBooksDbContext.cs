e9dbeac3909a91d9f9e53d34a4215720ba22a6b1
valerieturgeon <valerie.turgeon.cem@gmail.com>
2021-09-21 00:00:38 -0400

    CONF Configuration EntityFramwork
    Installation des packages minimum pour EntityFramework et SQL Server, création du ApplicationDbContext et ajout de DI du service SQL Server et de la connexion à la BD.

---
+++ CrazyBooks/CrazyBooks/Models/Data/CrazyBooksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrazyBooks.Models.Data
+{
+  public class CrazyBooksDbContext: DbContext
+  {
+    public CrazyBooksDbContext(DbContextOptions<CrazyBooksDbContext> options) : base(options)
+    {
+
+    }
+
+  }
+}
