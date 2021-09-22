7791c3721287a61f144fe9b78432c017e363ea54
valerieturgeon <valerie.turgeon.cem@gmail.com>
2021-09-21 22:54:23 -0400

    CONFIG i18n
    Configuration de l’internationalisation

--- CrazyBooks/CrazyBooks/Startup.cs
+++ CrazyBooks/CrazyBooks/Startup.cs
@@ -5,12 +5,16 @@ using CrazyBooks_DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,9 +22,17 @@ namespace CrazyBooks
 {
   public class Startup
   {
+    private CultureInfo[] supportedCultures = new[]
+            {
+                new CultureInfo("en-US"),
+                new CultureInfo("fr-CA"),
+                new CultureInfo("es-ES")
+            };
+
     public Startup(IConfiguration configuration)
     {
       Configuration = configuration;
+
     }
 
     public IConfiguration Configuration { get; }
@@ -28,6 +40,26 @@ namespace CrazyBooks
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      #region Localization
+      services.AddLocalization(options => options.ResourcesPath = "Resources");
+      services.AddControllersWithViews().AddRazorRuntimeCompilation()
+              .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+              .AddDataAnnotationsLocalization();
+
+      /*.AddDataAnnotationsLocalization(options => {
+             options.DataAnnotationLocalizerProvider = (type, factory) =>
+                 factory.Create(typeof(SharedResource));
+             }); */
+
+      services.Configure<RequestLocalizationOptions>(options =>
+      {
+        options.DefaultRequestCulture = new RequestCulture(culture: "en-US", uiCulture: "en-US");
+        options.SupportedCultures = supportedCultures;
+        options.SupportedUICultures = supportedCultures;
+      });
+
+      #endregion
+
       services.AddDbContext<CrazyBooksDbContext>(options =>
       options.UseSqlServer(
         Configuration.GetConnectionString("DefaultConnection")));
@@ -40,6 +72,9 @@ namespace CrazyBooks
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+      var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+      app.UseRequestLocalization(locOptions.Value);
+
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
